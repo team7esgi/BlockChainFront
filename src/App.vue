d33c25862b8fe3fc422e73c8c937654dbbf21a36
John allou <allou.john@hotmail.fr>
2021-01-21 04:51:48 +0100

    premier link front/back

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,91 @@ export default {
     //
   }),
 };
+
+console.log("What's up !")
+
+const Web3 = require("web3")
+
+const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/59abde443d5642698d98c8c7f87877c4"))
+
+
+
+web3.eth.getBalance("0x3aaB78883AA1Dd88CbfF414e8A281Ad32930d07f", function(err, result) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(web3.utils.fromWei(result, "ether") + " ETH")
+  }
+})
+
+const myContractAddress = "0xecda1435f86f1d3d2438f5ef087c1acca32e5d37"
+const MY_ABI = [
+        {
+          "inputs": [],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "name": "deposits",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "seller",
+              "type": "address"
+            }
+          ],
+          "name": "deposit",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address payable",
+              "name": "seller",
+              "type": "address"
+            }
+          ],
+          "name": "withdraw",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ]
+function startApp(){
+
+  var myContract =new  web3.eth.Contract(MY_ABI, myContractAddress);
+  
+  //var test = myContract.at(myContractAddress);
+
+  console.log(myContract);
+
+
+}
+startApp()
+
+  
+
+
+
+
 </script>
